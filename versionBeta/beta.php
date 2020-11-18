9d3c50dabc66d75e99778efe60e19f9a8a2823fd
Felipe Cortez <hola@faco.cl>
2020-11-18 08:58:24 -0300

    Update beta.php

--- versionBeta/beta.php
+++ versionBeta/beta.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<html lang="en-us">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <?php 
-  $name = $_POST['name'];
-  $edad = date('Y')-($_POST['age']);
-  ?>
-
-    <title>CreaGuemil | <?php print($name);?> | <?php print($edad);?> años</title>
+    <?php $name = $_POST['name']; $edad = date('Y')-($_POST['age']);?>
     <link rel="stylesheet" href="TemplateData/style.css">
     <link rel="icon" type="image/png" href="http://crea.guemil.info/TemplateData/favicon.png" /> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-
-<?php
-if (isset($_SERVER['HTTP_USER_AGENT'])) {
-    $agent = $_SERVER['HTTP_USER_AGENT'];
-}
-
-if (strlen(strstr($agent, 'Firefox')) > 0) { ?>
-<style>
-  #unity-webgl-logo{display:none;}
-</style>
-<?php };?>
-
+    <?php 
+    if (isset($_SERVER['HTTP_USER_AGENT'])) { $agent = $_SERVER['HTTP_USER_AGENT']; }
+    if (strlen(strstr($agent, 'Firefox')) > 0) { ?>
+    <style>#unity-webgl-logo{display:none;}</style>
+    <?php };?>
+    <title>CreaGuemil | <?php print($name);?> | <?php print($edad);?> años</title>
+    <!-- Metadata -->    
+    <meta name="title" content="CreaGuemil" />  
+    <meta name="description" content="CreaGuemil es parte de una iniciativa abierta de íconos, orientada a representar situaciones de riesgo y emergencia">
+    <meta name="image" content="https://crea.guemil.info/creaguemil.png">  
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="CreaGuemil" />
+    <meta name="twitter:description" content="CreaGuemil es parte de una iniciativa abierta de íconos, orientada a representar situaciones de riesgo y emergencia" />
+    <meta name="twitter:image:src" content="https://crea.guemil.info/creaguemil.png">
+    <!-- Open Graph data -->
+    <meta name="og:url" content="https://crea.guemil.info" />
+    <meta name="og:site_name" content="CreaGuemil" />
+    <meta name="og:description" content="CreaGuemil es parte de una iniciativa abierta de íconos, orientada a representar situaciones de riesgo y emergencia" />
+    <meta name="og:image" content="https://crea.guemil.info/creaguemil.png">
   </head>
   <body>
     <div id="unity-container" class="unity-desktop">
@@ -53,13 +55,11 @@ if (strlen(strstr($agent, 'Firefox')) > 0) { ?>
         productName: "Plataforma Guemil",
         productVersion: "1.0",
       };
-
       var container = document.querySelector("#unity-container");
       var canvas = document.querySelector("#unity-canvas");
       var loadingBar = document.querySelector("#unity-loading-bar");
       var progressBarFull = document.querySelector("#unity-progress-bar-full");
       var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-
       if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
         container.className = "unity-mobile";
       } else {
@@ -67,7 +67,6 @@ if (strlen(strstr($agent, 'Firefox')) > 0) { ?>
         canvas.style.height = "600px";
       }
       loadingBar.style.display = "block";
-
       var script = document.createElement("script");
       script.src = loaderUrl;
       script.onload = () => {
